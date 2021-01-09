2f04b9869c9e9427632854afe7fd098a7d9763d9
afidah-ser <71678377+afidah-ser@users.noreply.github.com>
2021-01-09 10:52:10 +0700

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-var n = prompt("Tulis Namamu:");
-var c = confirm("Hello " + n + "Selamat Datang di Taman Edukasi Embung Park");
-if (c == true) {
-    alert('Good Luck');
-} else {
-    alert('No Problem');
-}
